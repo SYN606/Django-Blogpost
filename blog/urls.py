5a3cd6630fbe22822ba350792fe736b1ec886853
SYN <SYN@mail.com>
2022-11-17 19:00:28 +0530

    created a page for viewing blogs

--- blog/urls.py
+++ blog/urls.py
@@ -4,9 +4,10 @@ from . import views # |> instead of this using class based url patterns
 # from .views import HomeView
 
 urlpatterns = [
-# path('',views.home, name="home")
 
-path('', views.HomeView.as_view(), name='home'),
-path('article/<int:pk>', views.ArticleDetailView.as_view(), name='article')
+#   path('',views.home, name="home")
+
+    path('', views.HomeView.as_view(), name='home'),
+    path('article/<int:pk>', views.ArticleDetailView.as_view(), name='article')
 
 ]
